182de331910e5c510f97dbb49767dda799a23bf9
Fliwer <p.morthier@gmail.com>
2026-09-14 13:44:41 +0200

    Page d'inscription : formulaire connecté à l'API register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,69 @@
 'use client' // commme les components par défaut dans un fichier app/ est un server component 'use client ' permet de transformer en client component et seuls les clients componenets peuvent utiliser useState, onClick, onChange, ect
 
-import { use, useState } from "react";
+import { useState } from "react";
 
 export default function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [displayName, setDisplayName] = useState('');
-    const [birthdate, setBirthdate] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+
+    // React.SubmitEvent = le type de l'événement "soumission de formulaire"
+    // (React.FormEvent est déconseillé/déprécié dans cette version : "n'existe pas vraiment")
+    // e contient les infos sur cet événement précis (ici : la soumission)
+    
+    const handleSubmit = async (e: React.SubmitEvent) => {
+        e.preventDefault();
+
+        const reponse = await fetch('http://localhost:8080/api/auth/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            // Transforme un objet javascript en TEXTE json
+            body: JSON.stringify({
+                email,
+                password,
+                display_name: displayName,
+                birth_date: birthDate,
+            }),
+        });
+
+        const donnees = await reponse.json();
+        console.log(donnees);
+    };
 
     return (
-        <h1>Inscription</h1>
-    )
+        <form onSubmit={handleSubmit}>
+            <h1>Inscription</h1>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Mot de passe"
+            />
+            <input
+                type="text"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+                placeholder="Prénom"
+            />
+            <input
+                type="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+            />
+
+            <button type="submit">S'inscrire</button>
+
+        </form>
+    );
+
+
+
+
 }
